d428a5bdc54e534b168e710ccdc56c2ddaf34c5e
maxumka1792 <maxumka1792@gmail.com>
2021-03-23 19:26:37 +0700

    Add pagination

---
+++ CarPriceAppWeb/Infrastructure/Helpers/IEnumerableExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CarPriceAppWeb.Infrastructure.Helpers
+{
+    public static class IEnumerableExtension
+    {
+        public static PagintaionEnumerable<T> ToPaginationEnumerable<T>(this IEnumerable<T> items, int pageSize) 
+            => new(items, pageSize);
+    }
+}
---
+++ CarPriceAppWeb/Infrastructure/Helpers/PagintaionEnumerable.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CarPriceAppWeb.Infrastructure.Helpers
+{
+    public class PagintaionEnumerable<T> : IEnumerable<T>
+    {
+        private readonly T[] _items;
+
+        private readonly int _totalPages;
+
+        private readonly int _pageSize;
+
+        private int _pageNumber = 1;
+
+        private bool HasBackPage => _pageNumber > 1;
+
+        private bool HasNextPage => _pageNumber <= _totalPages;
+
+        public PagintaionEnumerable(IEnumerable<T> items, int pageSize)
+        {
+            if (items is null) throw new ArgumentNullException(nameof(items));
+
+            if (pageSize <= 0) throw new ArgumentException("was zero or negative", nameof(pageSize));
+
+            _items = items.ToArray();
+            _pageSize = pageSize;
+
+            _totalPages = (int)Math.Ceiling(_items.Length / (decimal)_pageSize);
+        }
+
+        public void NextPage()
+        {
+            _pageNumber++;
+
+            if (!HasNextPage)
+            {
+                _pageNumber--;
+            }
+        }
+
+        public void BackPage()
+        {
+            _pageNumber--;
+
+            if (!HasBackPage)
+            {
+                _pageNumber = 1;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var fromIndex = (_pageNumber - 1) * _pageSize;
+
+            var toIndex = (_pageNumber * _pageSize) > _items.Length
+                        ? _items.Length
+                        : _pageNumber * _pageSize;
+
+            for (var index = fromIndex; index < toIndex; index++)
+            {
+                yield return _items[index];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
